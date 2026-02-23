3522d684211fa0a25211aa22da2b44f4a4df91cf
sofiagen8 <c26sofia.gentile@afacademy.af.edu>
2026-02-23 11:20:14 -0700

    fixed VHDL issue

--- src/sevenseg_decoder.vhd
+++ src/sevenseg_decoder.vhd
@@ -3,9 +3,9 @@
 -- Engineer: 
 -- 
 -- Create Date: 02/23/2026 09:59:36 AM
--- Design Name: 
+-- Designer Name: Sofia Gentile
 -- Module Name: sevenseg_decoder - Behavioral
--- Project Name: 
+-- Project Name: Seven Segment Decoder
 -- Target Devices: 
 -- Tool Versions: 
 -- Description: 
@@ -32,13 +32,31 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity sevenseg_decoder is
-    Port ( i_Hex : in STD_LOGIC_VECTOR (3 downto 0);
-           o_seg_n : out STD_LOGIC_VECTOR (6 downto 0));
+    Port ( i_Hex : in std_logic_vector(3 downto 0);
+            o_seg_n : out STD_LOGIC_VECTOR (6 downto 0));
 end sevenseg_decoder;
+
 
 architecture Behavioral of sevenseg_decoder is
 
 begin
-
+    with i_Hex select
+    o_seg_n <=  "1000000" when x"0",
+            "1111001" when x"1", --read truth table backwards, from Sg to Sa, and invert 0s and 1s for low
+            "0100100" when x"2", 
+            "0110000" when x"3", 
+            "0011001" when x"4",
+            "0010010" when x"5",
+            "0000010" when x"6",
+            "1111000" when x"7",
+            "0000000" when x"8",
+            "0011000" when x"9",
+            "0001000" when x"A",
+            "0000011" when x"B",
+            "0100111" when x"C",
+            "0100001" when x"D",
+            "0000110" when x"E",
+            "0001110" when x"F",
+            "0000" when others; --if basys3 reads wrong, fix truth table here
 
 end Behavioral;
